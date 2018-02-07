e6ae181b0be54e24e0f861a38922b33b3c12aacf
Weihao Cao <github@weihaocao.com>
2018-02-07 23:16:59 +0800

    button bug fixed

--- STM32_Lab/GPIO_LED_TEMPLATE/HARDWARE/LED/led.c
+++ STM32_Lab/GPIO_LED_TEMPLATE/HARDWARE/LED/led.c
@@ -20,7 +20,7 @@ void LED_Init(void)
 	GPIO_SetBits(GPIOE,GPIO_Pin_7);
 	
 	//F10
- GPIO_InitStructure.GPIO_Pin=GPIO_Pin_1;
+ GPIO_InitStructure.GPIO_Pin=GPIO_Pin_14;
  GPIO_InitStructure.GPIO_Mode=GPIO_Mode_OUT;
  GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_UP;
--- STM32_Lab/GPIO_LED_TEMPLATE/USER/main.c
+++ STM32_Lab/GPIO_LED_TEMPLATE/USER/main.c
@@ -10,9 +10,9 @@ int main(void)
 	
 	LED_Init();
 	Button_Init();
-	
 	while(1){
-		if (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_10)){
+		if (!GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_10)){
+			
 			RED_LED_TOGGLE();
 			GREEN_LED_TOGGLE();
 		}
